0cd19d03394c7e7f29348e344932d5b702d6217e
cgesacion <cameron.gesacion@patelhs.org>
2020-10-09 16:23:50 +0000

    First off I added a scanner by putting in the phrase Scanner scan = new Scanner(System.in);

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+ 
+     Scanner scan = new Scanner(System.in);
+
+     System.out.println("whats your name");
+     String name = scan.next();
+
+     System.out.println("whats your age?");
+     int age = scan.nextInt();
+
+     System.out.println("do you have siblings");
+     String siblings = scan.next();
+
+
   }
 }
