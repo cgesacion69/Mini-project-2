86b24c12eff2247324f2294062d97b5f3def28ce
cgesacion <cameron.gesacion@patelhs.org>
2020-10-16 17:34:13 +0000

    After the original scanner I added a bunch of print statements

--- Main.java
+++ Main.java
@@ -14,6 +14,22 @@ class Main {
      System.out.println("do you have siblings");
      String siblings = scan.next();
 
+     System.out.println("what time is it");
+     int time = 22;
+if (time < 10) {
+  System.out.println("the morning.");
+} else if (time < 20) {
+  System.out.println("the middle of the day.");
+} else {
+  System.out.println("the evening.");
+
+  System.out.println("can you count from 1-10");
+  
+  for(int i=1;i<=10;i++){
+      System.out.println(i);
+}
+
 
   }
+}
 }
